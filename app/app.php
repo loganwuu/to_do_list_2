5d056daa573b27aef2e3fdbb0388150e2a4ec271
Jordan Johansen & Logan Wu <student@epicodus.com>
2015-08-05 11:39:39 -0700

    adding twig framework complete.

--- app/app.php
+++ app/app.php
@@ -9,59 +9,26 @@
     }
 
     $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
 
-    $app->get("/", function() {
+    $app->get("/", function() use ($app) {
 
-        $output = "";
+        return $app['twig']->render('tasks.twig', array('tasks' => Task::getAll()));
 
-        if (!empty(Task::getAll())) {
-            $output .= "
-                <h1>To Do List</h1>
-                <p>Here are all your tasks:</p>
-            <ul>";
-
-            foreach (Task::getAll() as $task) {
-                $output .= "<p>" . $task->getDescription() . "</p>";
-            }
-
-            $output .= "</ul>";
-        }
-
-        $output .= "
-            <form action='/tasks' method='post'>
-                <label for='description'>Task Description</label>
-                <input id='description' name='description' type='text'>
-                <button type='submit'>Add task</button>
-            </form>
-            ";
-
-        $output .= "
-            <form action='/delete_tasks' method='post'>
-                <button type='submit'>delete</button>
-            </form>
-        ";
-
-        return $output;
     });
 
-    $app->post("/tasks", function() {
+    $app->post("/tasks", function() use ($app) {
         $task = new Task($_POST['description']);
         $task->save();
-        return "
-            <h1>You created a task!</h1>
-            <p>" . $task->getDescription() . "</p>
-            <p><a href='/'>View your list of things to do.</a></p>
-        ";
+        return $app['twig']->render('create_task.twig', array('newtask' => $task));
     });
 
-    $app->post("/delete_tasks", function() {
-
+    $app->post("/delete_tasks", function() use ($app) {
         Task::deleteAll();
+        return $app['twig']->render('delete_tasks.twig');
 
-        return "
-            <h1>List Cleared!</h1>
-            <p><a href='/'>Home</a></p>
-        ";
     });
 
     return $app;
