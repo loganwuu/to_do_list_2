13a45eab21d684b70568d9d86f6b4d9bd23b36ac
logan wu & mike rapp <student@epicodus.com>
2015-08-17 11:33:49 -0700

    add static function getAll()

--- src/Task.php
+++ src/Task.php
@@ -22,5 +22,17 @@ class Task
     {
         $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
     }
+
+    static function getAll()
+    {
+        $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
+        $tasks = array();
+        foreach($returned_tasks as $task) {
+            $description = $task['description'];
+            $new_task = new Task($description);
+            array_push($tasks, $new_task);
+        }
+        return $tasks;
+    }
 }
 ?>
